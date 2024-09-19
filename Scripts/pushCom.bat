644187ee33816650d01487bd3d8398fb5b0e2921
Halvor Nafstad <halnaf@attensi.com>
2024-09-19 14:08:15 +0200

    echo commands

--- Scripts/pushCom.bat
+++ Scripts/pushCom.bat
@@ -1,18 +1,22 @@
+@echo off
 setlocal
 
 REM Add all changes to the staging area
+echo git add .
 git add .
 
 REM Commit with the message passed as an argument
+echo git commit -m "%*"
 git commit -m "%*"
 
-@echo off
 REM Check if the current branch has an upstream branch
 git rev-parse --abbrev-ref --symbolic-full-name @{u} >nul 2>&1
 if %errorlevel% neq 0 (
-    echo No upstream branch, setting upstream and pushing...
+    echo No upstream branch
+    echo git push --set-upstream origin HEAD
     git push --set-upstream origin HEAD
 ) else (
+    echo git push
     git push
 )
 
