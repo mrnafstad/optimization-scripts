15066386df7fbd7a770a578beb8cb6b3af7429ca
Halvor Nafstad <halnaf@attensi.com>
2024-11-04 09:58:17 +0100

    reformat

--- Scripts/saveLastCommit.bat
+++ Scripts/saveLastCommit.bat
@@ -26,7 +26,7 @@ for /f "tokens=1,2*" %%i in ('git diff-tree --no-commit-id --name-status -r %LAS
     set "FILE_NAME=%%j"
     
     :: Output the file name with its status description, indented
-    echo     !FILE_NAME! ( !FILE_STATUS! ) >> temp_diff_files_formatted.txt
+    echo !FILE_STATUS!    !FILE_NAME! >> temp_diff_files_formatted.txt
 
 :: Step 6: Save the information in a formatted way to the output file
 (
